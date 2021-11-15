36d62c5648ab366b2e325bdb5e7b73a96e43fecc
Santiago Lopez <sanhurta2002@gmail.com>
2021-11-15 17:04:13 -0500

    adding neo

--- script.js
+++ script.js
@@ -1,34 +1,99 @@
-let apiKey = 'fD430FM8268SGoczkLpoeuMNN6Ah7aWvBWaYZRj5';
+const apiKey = "fD430FM8268SGoczkLpoeuMNN6Ah7aWvBWaYZRj5";
 
+const getAPOTD = async () => {
+  await fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`)
+    .then((res) => {
+      return res.json();
+    })
+    .then((dataAPOTD) => {
+      const imageContainer = document.getElementById("imageContainer");
+      const copyright = document.getElementById("copyright");
+      if (dataAPOTD.media_type === "video") {
+        imageContainer.innerHTML += `<iframe src='${dataAPOTD.url}'  frameBorder="0"  allowFullScreen id='videoOfDay'></iframe>`;
+      } else {
+        imageContainer.innerHTML += `<img src='${dataAPOTD.url}' id='imageOfDay'>`;
+      }
+      copyright.innerHTML += `<h2 id = 'authorName' > ${dataAPOTD.title}</h2><p id='textOfDay'>${dataAPOTD.explanation}</p > <p id ='date' > ${dataAPOTD.date} </p>`;
+    });
+};
 
-async function getAPOTD() {
-    let imageContainer = document.getElementById('imageContainer');
-    let copyright = document.getElementById('copyright');
-    let response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`);
-    let data = await response.json();
-    let mediaType = data.media_type;
-    console.log(data);
-    console.log(mediaType);
-    if (mediaType === 'video') {
-        imageContainer.innerHTML += `<iframe src='${data.url}'  frameBorder="0"  allowFullScreen id='videoOfDay'></iframe>`;
-    } else {
-        imageContainer.innerHTML += `<img src='${data.url}' id='imageOfDay'>`;
-    }
-    copyright.innerHTML += `<h2 id = 'authorName' > ${ data.title }</h2><p id='textOfDay'>${data.explanation}</p > <p id ='date' > ${ data.date } </p>`;
+getAPOTD();
+let neoArticle = document.getElementById("neoArticle");
+let divNEO = document.getElementById("neo");
+
+const getNEO = async () => {
+  let currentDate = new Date();
+  let date =
+    currentDate.getFullYear() +
+    "-" +
+    (currentDate.getMonth() + 1) +
+    "-" +
+    currentDate.getDate();
+  await fetch(
+    `https://api.nasa.gov/neo/rest/v1/feed?start_date=${date}&api_key=${apiKey}`
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((dataNeo) => {
+      const elementCount = dataNeo.element_count;
+      const neo = dataNeo.near_earth_objects;
+      const valuesNEO = Object.values(neo);
+      valuesNEO.forEach((items) => {
+        divNEO.innerHTML += `<div class="dropdown">
+                                    <button onclick="myFunction()" class="dropbtn">
+                                        ${items}
+                                    </button>
+                                    <div id="myDropdown" class="dropdown-content">
+                                    </div>
+                                </div>`;
+      });
+      let x = 0
+      console.log(valuesNEO);
+      console.log(valuesNEO[0][x].id)
+      for (let i = 0; i < valuesNEO.length;  i++) {
+          x++
+          for (let ii = 0; ii < .length; ii++) {
+              console.log(valuesNEO[0][x]);
+              
+          }
+      }
+    });
+};
+getNEO();
+
+let marsArticle = document.getElementById("marsArticle");
+let container = document.getElementById("container");
+let marsWeather = document.getElementById("marsWeather");
+let marsIframe = document.getElementById("marsIframe");
+marsArticle.addEventListener("click", () => {
+  marsWeather.style.display = "block";
+  marsWeather.innerHTML +=
+    "<iframe src='https://mars.nasa.gov/layout/embed/image/mslweather/' width='800' height='530' scrolling='no' frameborder='0' id='marsIframe'></iframe>";
+  container.style.filter = "blur(5px)";
+});
+
+neoArticle.addEventListener("click", () => {
+  divNEO.style.display = "inline-grid";
+  container.style.filter = "blur(5px)";
+});
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
 }
 
-let marswe = document.getElementById('earthArticle');
-let kk = document.getElementById('marsIframe');
-let container = document.getElementById('container')
-let marsWeather = document.getElementById('marsWeather')
-marswe.addEventListener('click', () => {
-
-    marsWeather.style.display = 'block';
-    container.style.filter = 'blur(5px)';
-
-})
-
-
-
-
-window.onload = getAPOTD();
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
